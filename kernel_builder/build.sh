1923c2d978197172147e401e39c324f1fdf9bbe2
Funs <fozzielumpkins@gmail.com>
2019-09-10 07:16:48 -0700

    rsync missing, also cleaned opts, not required

--- kernel_builder/build.sh
+++ kernel_builder/build.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 set -e
 set -x
-for i in `cat ../envfile | awk '{ print $2 }'`
-do
-  OPTS+=" --build-arg ENV $i"
-done
 
 if [ "$ver" == "" ]; then
   ver=$(basename $(pwd))
 fi
-docker build -t $ver . $OPTS
+docker build -t $ver . --build-arg $( grep apt_proxy ../envfile )
 # docker rmi $ver
