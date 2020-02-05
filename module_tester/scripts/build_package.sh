340d01377e83be6417031d7781521a578a7ad779
Funs <fozzielumpkins@gmail.com>
2020-02-05 09:55:12 -0800

    add packaging notifier of supported packaging

--- module_tester/scripts/build_package.sh
+++ module_tester/scripts/build_package.sh
@@ -14,13 +14,15 @@ if [ -f "$osr" ]; then
         dist=$ID_LIKE
     fi
 fi
-if [ "$dist" != "debian" ]; then
+if [ "$dist" == "debian" ]; then
   cd iomemory-vsl
   dpkg-buildpackage \
     -rfakeroot --no-check-builddeps --no-sign
-else
+elif [ "$dist" == "rhel" ]; then
   cd iomemory-vsl
   rpmbuild -ba fio-driver.spec
+else
+  echo "PHASE 1.3: Packaging on $dist is not suported."
 fi
 
 if [ "$?" != "0" ]; then
